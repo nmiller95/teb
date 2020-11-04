1c47254923164b13f80cb6f08f20d5ba2b85f406
Nikki Miller <nikkeh.miller@gmail.com>
2020-11-04 23:54:06 +0000

    Added frp function, fitcol now supports lin+quad fitting

--- flux_ratio_priors.py
+++ flux_ratio_priors.py
@@ -4,6 +4,7 @@ from astropy import units as u
 from astroquery.xmatch import XMatch
 from scipy.stats.mstats import theilslopes
 from scipy.optimize import minimize
+from uncertainties import ufloat
 
 t_hdu1 = Table.read('GCS3_WISE.fits', hdu=1)
 t_hdu2 = Table.read('GCS3_WISE.fits', hdu=2)
@@ -38,7 +39,7 @@ def mad(p, x, y):
     return np.mean(abs(y - p[0] - p[1] * x - p[2] * x ** 2))
 
 
-def fitcol(band, Tbl, Tref=5777):
+def fitcol(band, Tbl, Tref=5777, method='quad'):
     # dictionary of R values from Yuan et al., MNRAS 430, 2188–2199 (2013)
     # extrapolated/guessed for w3 and w4 - negligible for E(B-V)<0.05 anyway
     R = {'Jmag': 0.72, 'Hmag': 0.46, 'Kmag': 0.306,
@@ -51,12 +52,64 @@ def fitcol(band, Tbl, Tref=5777):
     x = x[i]
     y = y[i]
     p = theilslopes(y, x)  # starting values for optimiser
-    p = np.array([p[1], p[0], 0])
-    result = minimize(mad, p, args=(x, y), method='Nelder-Mead')
-    p = result.x
-    y0 = p[0] + p[1] * x + p[2] * x ** 2
-    res = abs(y - y0)
-    med = np.median(np.abs(res))  # warning comes from this line
-    i = np.abs(res) < 5 * med
-    rms = res[i].std()
-    return p[0], p[1], p[2], result.fun
+
+    if method == 'lin':
+        y0 = p[1] + p[0] * x
+        res = abs(y - y0)
+        med = np.median(np.abs(res))
+        i = np.abs(res) < 5 * med
+        rms = res[i].std()
+        return p[1], p[0], rms
+    elif method == 'quad':
+        p = np.array([p[1], p[0], 0])
+        result = minimize(mad, p, args=(x, y), method='Nelder-Mead')
+        p = result.x
+        y0 = p[0] + p[1] * x + p[2] * x ** 2
+        res = abs(y - y0)
+        med = np.median(np.abs(res))  # warning comes from this line
+        i = np.abs(res) < 5 * med
+        rms = res[i].std()
+        return p[0], p[1], p[2], result.fun
+    else:
+        print('incorrect')
+
+
+def calculate_flux_ratio_priors(Vrat, Teff1, Teff2, table1, table2, Tref1, Tref2, method='quad'):
+    data = {}
+    bands = ['Jmag', 'Hmag', 'Kmag', 'W1mag', 'W2mag', 'W3mag', 'W4mag']
+    tags = ['J', 'H', 'Ks', 'W1', 'W2', 'W3', 'W4']
+
+    if method == 'lin':
+        for band, tag in zip(bands, tags):
+            c1, m1, r1 = fitcol(band, table1, Tref1, method='lin')
+            c2, m2, r2 = fitcol(band, table2, Tref2, method='lin')
+            data[tag] = {'c1': c1, 'm1': m1, 'r1': r1, 'c2': c2, 'm2': m2, 'r2': r2}
+        # Return a dictionary of ufloat priors on flux ratios
+        d = {}
+        for b in data.keys():
+            col1 = data[b]['c1'] + data[b]['m1'] * (Teff1 - Tref1) / 1000.0
+            col2 = data[b]['c2'] + data[b]['m2'] * (Teff2 - Tref2) / 1000.0
+            L = Vrat * 10 ** (0.4 * (col2 - col1))
+            e_L = np.hypot(data[b]['r1'], data[b]['r2'])
+            d[b] = ufloat(L, e_L)
+        return d
+
+    elif method == 'quad':
+        for band, tag in zip(bands, tags):
+            p01, p11, p21, r1 = fitcol(band, table1, Tref1, method='quad')
+            p02, p12, p22, r2 = fitcol(band, table2, Tref2, method='quad')
+            data[tag] = {'p01': p01, 'p11': p11, 'p21': p21, 'r1': r2,
+                         'p02': p02, 'p12': p12, 'p22': p22, 'r2': r2}
+        # Return a dictionary of ufloat priors on flux ratios
+        d = {}
+        for b in data.keys():
+            col1 = data[b]['p01'] + data[b]['p11'] * (Teff1 - Tref1) / 1000.0
+            + data[b]['p21'] * ((Teff1 - Tref1) / 1000.0) ** 2
+            col2 = data[b]['p02'] + data[b]['p12'] * (Teff2 - Tref2) / 1000.0
+            + data[b]['p22'] * ((Teff2 - Tref2) / 1000.0) ** 2
+            L = Vrat * 10 ** (0.4 * (col2 - col1))
+            e_L = np.hypot(data[b]['r1'], data[b]['r2'])
+            d[b] = ufloat(L, e_L)
+        return d
+    else:
+        print('incorrect - method is lin or quad only')
