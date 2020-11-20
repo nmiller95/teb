65c1dba07279413b580d993a8cad291b828d40bc
Nikki Miller <nikkeh.miller@gmail.com>
2020-11-20 02:24:50 +0000

    Renamed some bands

--- flux_ratios.py
+++ flux_ratios.py
@@ -2,13 +2,13 @@ from astropy.table import Table
 from uncertainties import ufloat
 
 response_files = {
-    # Gaia and Johnson are in different format
+    # Gaia and Johnson are in different format. Need to include Johnson as minimum
     'FUV': 'Response/EA-fuv_im.tbl',
     'NUV': 'Response/EA-nuv_im.tbl',
-    'stromgren_u': 'Response/u_Bessell2005.csv',
-    'stromgren_v': 'Response/v_Bessell2005.csv',
-    'stromgren_b': 'Response/b_Bessell2005.csv',
-    'stromgren_y': 'Response/y_Bessell2005.csv',
+    'u_stromgren': 'Response/u_Bessell2005.csv',
+    'v_stromgren': 'Response/v_Bessell2005.csv',
+    'b_stromgren': 'Response/b_Bessell2005.csv',
+    'y_stromgren': 'Response/y_Bessell2005.csv',
     'J': 'Response/sec6_4a.tbl1.dat',
     'H': 'Response/sec6_4a.tbl2.dat',
     'Ks': 'Response/sec6_4a.tbl3.dat',
@@ -16,12 +16,12 @@ response_files = {
     'W2': 'Response/RSR-W2.txt',
     'W3': 'Response/RSR-W3.txt',
     'W4': 'Response/RSR-W4.txt',
-    'skymapper_u': 'SkyMapper_SkyMapper.u.dat',
-    'skymapper_v': 'SkyMapper_SkyMapper.v.dat',
-    'skymapper_g': 'SkyMapper_SkyMapper.g.dat',
-    'skymapper_r': 'SkyMapper_SkyMapper.r.dat',
-    'skymapper_i': 'SkyMapper_SkyMapper.i.dat',
-    'skymapper_z': 'SkyMapper_SkyMapper.z.dat'
+    'u_skymapper': 'SkyMapper_SkyMapper.u.dat',
+    'v_skymapper': 'SkyMapper_SkyMapper.v.dat',
+    'g_skymapper': 'SkyMapper_SkyMapper.g.dat',
+    'r_skymapper': 'SkyMapper_SkyMapper.r.dat',
+    'i_skymapper': 'SkyMapper_SkyMapper.i.dat',
+    'z_skymapper': 'SkyMapper_SkyMapper.z.dat'
 }
 
 
