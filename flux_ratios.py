a677d7171ff383200579567d921785c5c6dabdf5
Nikki Miller <nikkeh.miller@gmail.com>
2020-11-20 02:11:14 +0000

    Response file dictionary filled out

--- flux_ratios.py
+++ flux_ratios.py
@@ -1,7 +1,28 @@
 from astropy.table import Table
 from uncertainties import ufloat
 
-response_files = {}
+response_files = {
+    # Gaia and Johnson are in different format
+    'FUV': 'Response/EA-fuv_im.tbl',
+    'NUV': 'Response/EA-nuv_im.tbl',
+    'stromgren_u': 'Response/u_Bessell2005.csv',
+    'stromgren_v': 'Response/v_Bessell2005.csv',
+    'stromgren_b': 'Response/b_Bessell2005.csv',
+    'stromgren_y': 'Response/y_Bessell2005.csv',
+    'J': 'Response/sec6_4a.tbl1.dat',
+    'H': 'Response/sec6_4a.tbl2.dat',
+    'Ks': 'Response/sec6_4a.tbl3.dat',
+    'W1': 'Response/RSR-W1.txt',
+    'W2': 'Response/RSR-W2.txt',
+    'W3': 'Response/RSR-W3.txt',
+    'W4': 'Response/RSR-W4.txt',
+    'skymapper_u': 'SkyMapper_SkyMapper.u.dat',
+    'skymapper_v': 'SkyMapper_SkyMapper.v.dat',
+    'skymapper_g': 'SkyMapper_SkyMapper.g.dat',
+    'skymapper_r': 'SkyMapper_SkyMapper.r.dat',
+    'skymapper_i': 'SkyMapper_SkyMapper.i.dat',
+    'skymapper_z': 'SkyMapper_SkyMapper.z.dat'
+}
 
 
 class FluxRatio:
