b42f69b8883f8590d94d39812a1a123396178ae4
Nikki Miller <nikkeh.miller@gmail.com>
2020-11-25 22:13:24 +0000

    Added docstring for lnprob, added option for single distortion function, fixed some formatting errors. Need to include new flux ratio priors stuff in the lnprob, though.

--- functions.py
+++ functions.py
@@ -6,50 +6,72 @@ from scipy.special import legendre
 from scipy.integrate import simps
 
 
-def flux_ratio_priors(Vrat, TeffF, TeffK):
-    # Vrat in the sense Flux_F/Flux_K
-    data = {
-        'J': {'cF': 0.919, 'mF': -0.408, 'sigF': 0.015, 'cK': 1.511, 'mK': -0.605, 'sigK': 0.018},
-        'H': {'cF': 1.118, 'mF': -0.549, 'sigF': 0.019, 'cK': 1.918, 'mK': -0.821, 'sigK': 0.027},
-        'Ks': {'cF': 1.181, 'mF': -0.564, 'sigF': 0.017, 'cK': 2.033, 'mK': -0.872, 'sigK': 0.025},
-        'W1': {'cF': 1.230, 'mF': -0.568, 'sigF': 0.027, 'cK': 2.094, 'mK': -0.865, 'sigK': 0.035},
-        'W2': {'cF': 1.234, 'mF': -0.547, 'sigF': 0.039, 'cK': 2.101, 'mK': -0.928, 'sigK': 0.062},
-        'W3': {'cF': 1.182, 'mF': -0.554, 'sigF': 0.021, 'cK': 2.062, 'mK': -0.907, 'sigK': 0.036},
-        'W4': {'cF': 1.225, 'mF': -0.519, 'sigF': 0.050, 'cK': 2.095, 'mK': -0.951, 'sigK': 0.060}
-    }
-    # Return a dictionary of ufloat priors on flux ratios
-    d = {}
-    for b in data.keys():
-        colF = data[b]['cF'] + data[b]['mF'] * (TeffF - 6400) / 1000.0
-        colK = data[b]['cK'] + data[b]['mK'] * (TeffK - 5200) / 1000.0
-        L = Vrat * 10 ** (0.4 * (colK - colF))
-        e_L = np.hypot(data[b]['sigF'], data[b]['sigK'])
-        d[b] = ufloat(L, e_L)
-    return d
+# def flux_ratio_priors(Vrat, TeffF, TeffK):
+#     # Vrat in the sense Flux_F/Flux_K
+#     data = {
+#         'J': {'cF': 0.919, 'mF': -0.408, 'sigF': 0.015, 'cK': 1.511, 'mK': -0.605, 'sigK': 0.018},
+#         'H': {'cF': 1.118, 'mF': -0.549, 'sigF': 0.019, 'cK': 1.918, 'mK': -0.821, 'sigK': 0.027},
+#         'Ks': {'cF': 1.181, 'mF': -0.564, 'sigF': 0.017, 'cK': 2.033, 'mK': -0.872, 'sigK': 0.025},
+#         'W1': {'cF': 1.230, 'mF': -0.568, 'sigF': 0.027, 'cK': 2.094, 'mK': -0.865, 'sigK': 0.035},
+#         'W2': {'cF': 1.234, 'mF': -0.547, 'sigF': 0.039, 'cK': 2.101, 'mK': -0.928, 'sigK': 0.062},
+#         'W3': {'cF': 1.182, 'mF': -0.554, 'sigF': 0.021, 'cK': 2.062, 'mK': -0.907, 'sigK': 0.036},
+#         'W4': {'cF': 1.225, 'mF': -0.519, 'sigF': 0.050, 'cK': 2.095, 'mK': -0.951, 'sigK': 0.060}
+#     }
+#     # Return a dictionary of ufloat priors on flux ratios
+#     d = {}
+#     for b in data.keys():
+#         colF = data[b]['cF'] + data[b]['mF'] * (TeffF - 6400) / 1000.0
+#         colK = data[b]['cK'] + data[b]['mK'] * (TeffK - 5200) / 1000.0
+#         L = Vrat * 10 ** (0.4 * (colK - colF))
+#         e_L = np.hypot(data[b]['sigF'], data[b]['sigK'])
+#         d[b] = ufloat(L, e_L)
+#     return d
 
 
-def lnprob(params,  # Model parameters and hyper-parameters
-           flux2mag,  # Magnitude data and flux-to-mag log-likelihood calculator
-           lratios,  # Flux ratios and responses
-           theta1, theta2,  # angular diameters as ufloats **in milli-arcseconds**
-           spec1, spec2,  # Model spectra
-           ebv_prior,  # ufloat
-           redlaw,  # Reddening law
-           Nc1,  # Number of distortion coeffs for star 1
-           wmin=1000, wmax=300000,
-           return_flux=False,
-           blobs=False, apply_flux_ratio_priors=True,
-           debug=False, verbose=False):
+def lnprob(params, flux2mag, lratios, theta1, theta2, spec1, spec2, ebv_prior, redlaw, Nc1,
+           wmin=1000, wmax=300000, return_flux=False, blobs=False, apply_flux_ratio_priors=True,
+           debug=False, verbose=False, single_dist_function=False):
+    """
+    Log probability function for the fundamental effective temperature of eclipsing binary stars method.
+
+    :param params: Model parameters and hyper-parameters as list. Should contain:
+        Teff1, Teff2, theta1, theta2, ebv, sigma_ext, sigma_l, sigma_c, [0]*(Nc1+Nc2)
+    :param flux2mag: Magnitude data and flux-to-mag log-likelihood calculator (Flux2Mag object)
+    :param lratios: Flux ratios and response functions (dictionary)
+    :param theta1: Angular diameter of primary star as ufloat in milli-arcseconds
+    :param theta2: Angular diamter of secondary star as ufloat in milli-arcseconds
+    :param spec1: Model spectrum of primary star (flint.ModelSpectrum object)
+    :param spec2: Model spectrum of secondary star (flint.ModelSpectrum object)
+    :param ebv_prior: Prior on E(B-V) as ufloat
+    :param redlaw: Reddening law (synphot.ReddeningLaw object)
+    :param Nc1: Number of distortion coefficients for primary star (star 1)
+    :param wmin: Lower wavelength cut for model spectrum, in Angstroms
+    :param wmax: Upper wavelength cut for model spectrum, in Angstroms
+    :param return_flux: Whether to return the wavelength, flux and distortion arrays
+    :param blobs: For the MCMC
+    :param apply_flux_ratio_priors: Whether to apply the flux ratio priors
+    :param debug: Whether to print extra stuff
+    :param verbose: Whether to print out all the parameters
+    :param single_dist_coefficient: If the 2 model spectra are very similar, can use 1 set of
+        distortion coefficients for both stars
+    :return: Either log likelihood and log prior, or wavelength, flux and distortion arrays
+    """
     SIGMA_SB = 5.670367E-5  # erg.cm-2.s-1.K-4
 
     Teff1, Teff2, Theta1, Theta2, ebv, sigma_ext, sigma_l, sigma_col = params[0:8]  # SIGMA_COL
 
-    if Theta1 < 0: return -np.inf
-    if Theta2 < 0: return -np.inf
-    if ebv < 0: return -np.inf
-    if sigma_ext < 0: return -np.inf
-    if sigma_col < 0: return -np.inf  # SIGMA_COL
-    if sigma_l < 0: return -np.inf
+    if Theta1 < 0:
+        return -np.inf
+    if Theta2 < 0:
+        return -np.inf
+    if ebv < 0:
+        return -np.inf
+    if sigma_ext < 0:
+        return -np.inf
+    if sigma_col < 0:
+        return -np.inf  # SIGMA_COL
+    if sigma_l < 0:
+        return -np.inf
 
     wave = spec1.waveset
     i = ((wmin * u.angstrom < wave) & (wave < wmax * u.angstrom)).nonzero()
@@ -73,23 +95,30 @@ def lnprob(params,  # Model parameters and hyper-parameters
     flux1 = flux1 * (1 + distort1)
     flux1 = flux1 / simps(flux1, wave)
 
-    distort2 = np.zeros_like(flux2)
-    for n, c in enumerate(params[8 + Nc1:]):
-        if abs(c) > 1: return -np.inf
-        distort2 = distort2 + c * legendre(n + 1)(x)
-    # Make distortion = 0 at 5556A (where Vega z.p. flux is defined)
-    distort2 = distort2 - distort2[i_5556]
-    if min(distort2) < -1:
-        return -np.inf
-    flux2 = flux2 * (1 + distort2)
-    flux2 = flux2 / simps(flux2, wave)
+    if single_dist_function:
+        flux2 = flux2 * (1 + distort1)
+        flux2 = flux2 / simps(flux2, wave)
+    else:
+        distort2 = np.zeros_like(flux2)
+        for n, c in enumerate(params[8 + Nc1:]):
+            if abs(c) > 1: return -np.inf
+            distort2 = distort2 + c * legendre(n + 1)(x)
+        # Make distortion = 0 at 5556A (where Vega z.p. flux is defined)
+        distort2 = distort2 - distort2[i_5556]
+        if min(distort2) < -1:
+            return -np.inf
+        flux2 = flux2 * (1 + distort2)
+        flux2 = flux2 / simps(flux2, wave)
 
     extinc = redlaw.extinction_curve(ebv)(wave).value
     f_1 = 0.25 * SIGMA_SB * (Theta1 / 206264806) ** 2 * Teff1 ** 4 * flux1
     f_2 = 0.25 * SIGMA_SB * (Theta2 / 206264806) ** 2 * Teff2 ** 4 * flux2
     flux = (f_1 + f_2) * extinc
     if return_flux:
-        return wave, flux, f_1 * extinc, f_2 * extinc, distort1, distort2
+        if single_dist_function:
+            return wave, flux, f_1 * extinc, f_2 * extinc, distort1
+        else:
+            return wave, flux, f_1 * extinc, f_2 * extinc, distort1, distort2
 
     chisq, lnlike_m, lnlike_c = flux2mag(wave, flux, sigma_ext, sigma_col)  # SIGMA_COL
     if verbose:
@@ -142,8 +171,10 @@ def lnprob(params,  # Model parameters and hyper-parameters
     # Priors on IR flux ratios
     RV = flux2mag.R['V'](wave)
     lV = simps(RV * f_2, wave) / simps(RV * f_1, wave)
+    # IT WILL BREAK HERE
     frp = flux_ratio_priors(lV, Teff1, Teff2)
-    if verbose: print('Flux ratio priors:')
+    if verbose:
+        print('Flux ratio priors:')
     chisq_flux_ratio_priors = 0
     for b in frp.keys():
         RX = flux2mag.R[b](wave)
@@ -153,14 +184,16 @@ def lnprob(params,  # Model parameters and hyper-parameters
         if apply_flux_ratio_priors:
             wt = 1 / (frp[b].s ** 2 + sigma_l ** 2)
             lnprior += -0.5 * ((lX - frp[b].n) ** 2 * wt - np.log(wt))
-    if verbose: print('Flux ratio priors: chi-squared = {:0.2}'.format(chisq_flux_ratio_priors))
+    if verbose:
+        print('Flux ratio priors: chi-squared = {:0.2}'.format(chisq_flux_ratio_priors))
 
     if debug:
         f = "{:0.1f} {:0.1f} {:0.4f} {:0.4f} {:0.4f} {:0.1f} {:0.4f}"
         f += " {:0.1e}" * Nc1
-        Nc2 = len(params) - 8 - Nc1
-        f += " {:0.1e}" * Nc2
-        f += " {:0.2f}"
+        if not single_dist_function:
+            Nc2 = len(params) - 8 - Nc1
+            f += " {:0.1e}" * Nc2
+            f += " {:0.2f}"
         print(f.format(*(tuple(params) + (lnlike,))))
     if np.isfinite(lnlike):
         if blobs:
