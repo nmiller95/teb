dd9dbbc98bf7683d747a2a5dadcf9f783472a028
Nikki Miller <nikkeh.miller@gmail.com>
2021-05-07 16:03:06 +0100

    main file (?)

---
+++ teb.py
@@ -0,0 +1,6 @@
+"""
+Main file for running all this nonsense
+Module will eventually be named teb (temperatures of eclipsing binaries)
+TODO Look into how best to get this working: command line based or something to read into scripts?
+
+"""
