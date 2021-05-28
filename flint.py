6b3f89e3f4ec669f355d8e23164eb8497aa99e36
Nikki Miller <nikkeh.miller@gmail.com>
2021-05-28 16:02:28 +0100

    major rewrite finished, ready for testing

--- flint.py
+++ flint.py
@@ -10,43 +10,98 @@ from scipy.special import legendre
 import warnings
 import os
 import pyvo as vo
-from math import ceil, floor
 
 
 __all__ = ['SpectralEnergyDistribution', 'ModelSpectrum', 'Bandpass',
            'DistortionPolynomial']
 
 
+def make_tag(params):
+    """
+    Makes unique tag to use in saving the model as a local file
+    :param params: :param params: Tuple containing teff, logg, m/h and a/Fe
+    :return: String unique to the model
+    """
+    teff, logg, m_h, afe = params
+    if m_h > 0:
+        tfmt = "lte{:03.0f}-{:3.1f}+{:3.1f}a{:+3.1f}"
+        return tfmt.format(teff / 100, logg, m_h, afe)
+    else:
+        tfmt = "lte{:03.0f}-{:3.1f}-{:3.1f}a{:+3.1f}"
+        return tfmt.format(teff / 100, logg, abs(m_h), afe)
+
+
 def load_spectrum_as_table(s, params, source):
+    """
+    Attempts to read a specific model from a source catalog of models
+    :param s: Service object from VO
+    :param params: Tuple containing teff, logg, m/h and a/Fe
+    :param source: Name of model database being used. 'bt-settl' and 'coelho-sed' supported.
+    :return: astropy.Table containing model wavelength and flux
+    """
     teff, logg, m_h, afe = params
     cond_teff = s['teff'] == teff
     cond_logg = s['logg'] == logg
     cond_meta = s['meta'] == m_h
+    # Column names depend on source
     if source == 'bt-settl':
         cond_alpha = s['alpha'] == afe
     elif source == 'coelho-sed':
         cond_alpha = s['afe'] == afe
     else:
         raise NameError('model not supported')
+    # Restricts table to just the requested parameters
     s = s[cond_teff & cond_logg & cond_meta & cond_alpha]
     try:
         url = (s[0]['Spectrum']).decode("utf-8")
-    except AttributeError:
+    except IndexError:
         raise FileNotFoundError("Spectrum with specified parameters not found")
     url += '&format=ascii'
-    return Table.read(url, format='ascii.fast_no_header')
+    return Table.read(url, format='ascii.fast_no_header', names=('wave', 'flux'))
 
 
-def model_interpolate_teff(s, teff, logg, m_h, afe, source):
-    upper_teff = ceil(teff / 100) * 100
-    lower_teff = floor(teff / 100) * 100
+def nearest_teff_models(s, params):
+    """
+    Finds nearest temperature above and below the temperature specified
+    :param s: Service object from VO
+    :param params: Tuple containing teff, logg, m/h and a/Fe
+    :return: Upper and lower temperature that can be read into model
+    """
+    teff, _, _, _ = params
+    # Quick check to see if outside range (this is for coelho; bt-settl goes much higher/lower)
+    if teff < 3000 or teff > 26000:
+        raise ValueError("Temperature outside supported range. Must be 3000K <= Teff <= 26000K")
+    # Check which teffs are supported by the model
+    s_list = []
+    for val in s['teff']:
+        if val not in s_list:
+            s_list.append(val)
+    i = 0
+    while teff > s_list[i]:
+        i += 1
+    return s_list[i],  s_list[i-1]
+
+
+def model_interpolate_teff(s, params, source):
+    """
+    Loads 2 nearest models in temperature and does a simple linear interpolation
+    :param s: Service object from VO
+    :param params: Tuple containing teff, logg, m/h and a/Fe
+    :param source: Name of model database being used. 'bt-settl' and 'coelho-sed' supported.
+    :return: astropy.Table containing model wavelength and flux
+    """
+    teff, logg, m_h, afe = params
+    # Finds nearest two models in teff space
+    upper_teff, lower_teff = nearest_teff_models(s, params)
+    teff_range = upper_teff - lower_teff
     upper_params = (upper_teff, logg, m_h, afe)
     lower_params = (lower_teff, logg, m_h, afe)
     upper_model = load_spectrum_as_table(s, upper_params, source)
     lower_model = load_spectrum_as_table(s, lower_params, source)
-    wave = upper_model['col1']
-    flux = ((teff-lower_teff)/100)*upper_model['col2'] + ((upper_teff-teff)/100)*lower_model['col2']
-    return wave, flux
+    wave = upper_model['wave']
+    # Linear interpolation
+    flux = ((teff-lower_teff)/teff_range)*upper_model['flux'] + ((upper_teff-teff)/teff_range)*lower_model['flux']
+    return Table(wave, flux)
 
 
 class ModelSpectrum(SourceSpectrum):
@@ -60,10 +115,28 @@ class ModelSpectrum(SourceSpectrum):
     @classmethod
     def from_parameters(cls, teff, logg, m_h=0, afe=0, binning=10, reload=False,
                         source='bt-settl'):
-                        # version='CIFIST2011_2015'):
+        params = (teff, logg, m_h, afe)
 
         ############################################################
-        # very very rough example
+
+        tag = make_tag(params)
+        _f = "{}.{}.dat".format(tag, source)
+        model_file_0 = join(cls.cache_path, _f)
+        if binning is None:
+            model_file = model_file_0
+        else:  # Makes new pathname for a new file that's been binned up
+            ffmt = "{}.{}_{:04d}.fits"
+            model_file = join(cls.cache_path, ffmt.format(tag, source, int(binning)))
+
+        # If file exists (i.e. already downloaded and binned) and you don't want to re-download it, simples!
+        if os.path.isfile(model_file) and not reload:
+            return SourceSpectrum.from_file(model_file)
+
+        if binning is not None and os.path.isfile(model_file_0) and not reload:
+            t = Table.read(model_file_0)
+
+        ############################################################
+
         if source == 'bt-settl':
             service = vo.dal.SSAService("http://svo2.cab.inta-csic.es/theory/newov2/ssap.php?model=bt-settl&")
         elif source == 'coelho':
@@ -73,60 +146,33 @@ class ModelSpectrum(SourceSpectrum):
         # Read list of available models
         s = service.search()
         s = s.to_table()
-        if teff % 100:  # Temperature not in list: load 2 nearest and interpolate
-            wave, flux = model_interpolate_teff(s, teff, logg, m_h, afe, source)
-        else:
-            params = (teff, logg, m_h, afe)
-            model = load_spectrum_as_table(s, params, source)
-            wave = model['col1']
-            flux = model['col2']
-
-        ############################################################
-
-        subdir = {'CIFIST2011_2015': 'CIFIST2011'}
-        tag = cls.make_tag(teff, logg, m_h, afe, source)
-        # _f = "{}.BT-Settl.{}.fits".format(tag, version)
-        # fits_file_0 = join(cls.cache_path, _f)
-        if binning is None:
-            fits_file = fits_file_0
-        else:  # Makes new pathname for a new file that's been binned up
-            ffmt = "{}.BT-Settl.{}_{:04d}.fits"
-            fits_file = join(cls.cache_path, ffmt.format(tag, version,
-                                                         int(binning)))
-
-        # If file exists (i.e. already downloaded and binned) and you don't want to re-download it, simples!
-        if os.path.isfile(fits_file) and not reload:
-            return SourceSpectrum.from_file(fits_file)
-
-        if binning is not None and os.path.isfile(fits_file_0) and not reload:
-            t = Table.read(fits_file_0)
-
-        f_url = ("http://phoenix.ens-lyon.fr/Grids/BT-Settl/" +
-                 "{}/SPECTRA/{}.BT-Settl.spec.7.bz2")
-        url = f_url.format(subdir[version], tag)
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", UnitsWarning)
+        # Find and load model; interpolate if necessary
+        if source == 'coelho-sed':
             try:
-                t = Table.read(url, hdu=1, format='fits')
-            except OSError:
-                raise ValueError(url)
-        # t.remove_column('BBFLUX')
-        t.sort('WAVELENGTH')
-        t_g = t.group_by('WAVELENGTH')
-        t = t_g.groups.aggregate(np.mean)
-        t['FLUX'].unit = 'FLAM'
+                model = load_spectrum_as_table(s, params, source)
+            except FileNotFoundError:
+                model = model_interpolate_teff(s, params, source)
+        elif source == 'bt-settl' and teff % 100:
+            model = model_interpolate_teff(s, params, source)
+        else:
+            model = load_spectrum_as_table(s, params, source)
+
+        model.sort('wave')
+        model_g = model.group_by('wave')
+        model = model_g.groups.aggregate(np.mean)
+        model['flux'].unit = 'FLAM'  # TODO: check this is right for both sets of models
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", UnitsWarning)
-            if not os.path.isfile(fits_file_0):
-                t.write(fits_file_0)
-            if os.path.isfile(fits_file_0) and reload:
-                t.write(fits_file_0, overwrite=True)
+            if not os.path.isfile(model_file_0):
+                model.write(model_file_0)
+            if os.path.isfile(model_file_0) and reload:
+                model.write(model_file_0, overwrite=True)
             if binning is not None:
-                t.add_column(t['WAVELENGTH'] // int(binning), name='BIN')
-                t_b = t.group_by('BIN')
-                t = t_b.groups.aggregate(np.mean)
-            t.write(fits_file, overwrite=reload)
-        return SourceSpectrum.from_file(fits_file)
+                model.add_column(model['wave'] // int(binning), name='bin')
+                model_b = model.group_by('bin')
+                model = model_b.groups.aggregate(np.mean)
+            model.write(model_file, overwrite=reload)
+        return SourceSpectrum.from_file(model_file)
 
 
 class Bandpass(SpectralElement):
