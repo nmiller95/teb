a20354084c5d3b22c2f1c1876a050bb40679f465
Nikki Miller <nikkeh.miller@gmail.com>
2021-05-28 18:05:48 +0100

    added arbitrary process_spectrum to bin+exists-reload

--- flint.py
+++ flint.py
@@ -1,19 +1,20 @@
 import numpy as np
 from os.path import join, abspath, dirname
-from astropy.io.votable import parse
-from astropy.io.votable.exceptions import W10, W42
-from synphot import SpectralElement, Empirical1D, SourceSpectrum
-from synphot import BaseUnitlessSpectrum
+# from astropy.io.votable import parse
+# from astropy.io.votable.exceptions import W10, W42
+from synphot import SourceSpectrum  # , SpectralElement, Empirical1D
+# from synphot import BaseUnitlessSpectrum
 from astropy.table import Table
 from astropy.units import UnitsWarning
-from scipy.special import legendre
+# from scipy.special import legendre
 import warnings
 import os
 import pyvo as vo
 
 
-__all__ = ['SpectralEnergyDistribution', 'ModelSpectrum', 'Bandpass',
-           'DistortionPolynomial']
+# __all__ = ['SpectralEnergyDistribution', 'ModelSpectrum', 'Bandpass',
+#            'DistortionPolynomial']
+__all__ = ['ModelSpectrum']
 
 
 def make_tag(params):
@@ -172,6 +173,7 @@ class ModelSpectrum(SourceSpectrum):
         # Get un-binned file if already downloaded
         if binning is not None and os.path.isfile(model_file_0) and not reload:
             model = Table.read(model_file_0, format='ascii')
+            process_spectrum(model, model_file, model_file_0, reload, binning)
         else:
             if source == 'bt-settl':
                 service = vo.dal.SSAService("http://svo2.cab.inta-csic.es/theory/newov2/ssap.php?model=bt-settl&")
@@ -201,73 +203,73 @@ class ModelSpectrum(SourceSpectrum):
         return SourceSpectrum.from_file(model_file)
 
 
-class Bandpass(SpectralElement):
-    cache_path = join(dirname(abspath(__file__)), 'cache')
-    if not os.path.exists(cache_path):
-        os.mkdir(cache_path)
-    cache_path = join(cache_path, 'fps')
-    if not os.path.exists(cache_path):
-        os.mkdir(cache_path)
-
-    @classmethod
-    def from_svo(cls, filter_id, keep_neg=False, reload=False):
-
-        facility, filter_name = filter_id.split('/')
-        cls.facility = facility
-        cls.filter_name = filter_name
-
-        cache_path = join(cls.cache_path, facility)
-        if not os.path.exists(cache_path):
-            os.mkdir(cache_path)
-        xml_file = join(cache_path, filter_name + '.xml')
-
-        if os.path.isfile(xml_file) and not reload:
-            votable = parse(xml_file)
-        else:
-            _url = 'http://svo2.cab.inta-csic.es/svo/theory/fps/fps.php?ID={}'
-            url = _url.format(filter_id)
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", W42)
-                warnings.simplefilter("error", W10)
-                try:
-                    votable = parse(url)
-                except W10:
-                    raise ValueError('FilterID {} not found'.format(filter_id))
-            votable.to_xml(xml_file)
-
-        meta = dict()
-        for field in votable.iter_fields_and_params():
-            try:
-                meta[field.name] = float(field.value)
-            except:
-                pass
-            try:
-                meta[field.name] = field.value.decode()
-            except:
-                pass
-
-        thruput_table = votable.get_first_table().to_table()
-        wavelengths = thruput_table['Wavelength']
-        fluxes = thruput_table['Transmission']
-        return cls(Empirical1D, points=wavelengths, lookup_table=fluxes,
-                   keep_neg=keep_neg, meta=meta)
-
-
-class DistortionPolynomial(BaseUnitlessSpectrum):
-
-    @classmethod
-    def from_coeffs(cls, coeffs, wmin=500, wmax=500000, nwave=499501):
-        x = np.linspace(-1, 1, nwave)
-        w = wmin + 0.5 * (x + 1) * (wmax - wmin)
-        d = np.ones(nwave)
-        for n, c in enumerate(coeffs):
-            d += c * legendre(n + 1)(x)
-        return cls(Empirical1D, points=w, lookup_table=d, keep_neg=True)
-
-
-class SpectralEnergyDistribution(SourceSpectrum):
-
-    @classmethod
-    def from_model(cls, model_spectrum, reddening_law, distortion_polynomial,
-                   total_flux):
-        return cls(Empirical1D, np.ones(1001))
+# class Bandpass(SpectralElement):
+#     cache_path = join(dirname(abspath(__file__)), 'cache')
+#     if not os.path.exists(cache_path):
+#         os.mkdir(cache_path)
+#     cache_path = join(cache_path, 'fps')
+#     if not os.path.exists(cache_path):
+#         os.mkdir(cache_path)
+#
+#     @classmethod
+#     def from_svo(cls, filter_id, keep_neg=False, reload=False):
+#
+#         facility, filter_name = filter_id.split('/')
+#         cls.facility = facility
+#         cls.filter_name = filter_name
+#
+#         cache_path = join(cls.cache_path, facility)
+#         if not os.path.exists(cache_path):
+#             os.mkdir(cache_path)
+#         xml_file = join(cache_path, filter_name + '.xml')
+#
+#         if os.path.isfile(xml_file) and not reload:
+#             votable = parse(xml_file)
+#         else:
+#             _url = 'http://svo2.cab.inta-csic.es/svo/theory/fps/fps.php?ID={}'
+#             url = _url.format(filter_id)
+#             with warnings.catch_warnings():
+#                 warnings.simplefilter("ignore", W42)
+#                 warnings.simplefilter("error", W10)
+#                 try:
+#                     votable = parse(url)
+#                 except W10:
+#                     raise ValueError('FilterID {} not found'.format(filter_id))
+#             votable.to_xml(xml_file)
+#
+#         meta = dict()
+#         for field in votable.iter_fields_and_params():
+#             try:
+#                 meta[field.name] = float(field.value)
+#             except:
+#                 pass
+#             try:
+#                 meta[field.name] = field.value.decode()
+#             except:
+#                 pass
+#
+#         thruput_table = votable.get_first_table().to_table()
+#         wavelengths = thruput_table['Wavelength']
+#         fluxes = thruput_table['Transmission']
+#         return cls(Empirical1D, points=wavelengths, lookup_table=fluxes,
+#                    keep_neg=keep_neg, meta=meta)
+#
+#
+# class DistortionPolynomial(BaseUnitlessSpectrum):
+#
+#     @classmethod
+#     def from_coeffs(cls, coeffs, wmin=500, wmax=500000, nwave=499501):
+#         x = np.linspace(-1, 1, nwave)
+#         w = wmin + 0.5 * (x + 1) * (wmax - wmin)
+#         d = np.ones(nwave)
+#         for n, c in enumerate(coeffs):
+#             d += c * legendre(n + 1)(x)
+#         return cls(Empirical1D, points=w, lookup_table=d, keep_neg=True)
+#
+#
+# class SpectralEnergyDistribution(SourceSpectrum):
+#
+#     @classmethod
+#     def from_model(cls, model_spectrum, reddening_law, distortion_polynomial,
+#                    total_flux):
+#         return cls(Empirical1D, np.ones(1001))
