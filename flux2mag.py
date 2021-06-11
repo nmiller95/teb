d57d9532c13bf1f5ca16d4e3714f8f277867176f
Nikki Miller <nikkeh.miller@gmail.com>
2021-06-11 02:55:34 +0100

    fully documented

--- flux2mag.py
+++ flux2mag.py
@@ -10,9 +10,10 @@ from astroquery.simbad import Simbad
 
 class Flux2mag:
     """
-    Flux2mag methods:
-        __init__: Accesses and consolidates magnitude and photometric colors of a star.
-        __call__: Generates synthetic magnitudes and colors from integrating a reference spectrum of Vega.
+    Stores observed magnitudes and colours and calculates synthetic magnitudes and colours
+
+    __init__: Accesses and consolidates magnitude and photometric colors of a star.
+    __call__: Generates synthetic magnitudes and colors from integrating a reference spectrum of Vega.
     """
 
     def __init__(self, name, extra_data=None, colors_data=None):
@@ -20,40 +21,53 @@ class Flux2mag:
         Reads in bandpass data from package files. Calculates a standardised response function and pivot wavelength of
         each bandpass. Retrieves standard photometric data from SIMBAD and processes any extra data supplied.
 
-        :param name: Name of star, as understood by SIMBAD
-        :type name: basestring
-        :param extra_data: List of dictionaries containing information about non-standard magnitudes.
+        Parameters
+        ----------
+        name: str
+            Name of star, as understood by SIMBAD
+        extra_data: list, optional
+            List of dictionaries containing information about non-standard magnitudes.
             Each dictionary must contain:
-                'tag' - string, photometric band/system name/label
-                'mag' - ufloat, observed AB magnitude with standard error
-                'zp'  - ufloat, zero-point and standard error
-                'wave' - array, wavelengths for response function, ** in angstrom **
-                'resp'  - array, response function
-        :type extra_data: list or none
-        :param colors_data: List of dictionaries containing information about photometric colors.
+                * tag: str,
+                    Photometric band name/label (must be unique)
+                * mag: `uncertainties.ufloat`
+                    Observed AB magnitude with standard error
+                * zp: `uncertainties.ufloat`
+                    Photometric band zero-point and its standard error
+                * wave: array_like
+                    Wavelength array for response function, in angstrom
+                * resp: array_like
+                    Response function
+        colors_data: list, optional
+            List of dictionaries containing information about photometric colors.
             Each dictionary must contain:
-                'tag' - string, photometric color name/label (must be unique), e.g. '(b-y)_1'
-                'type' - string, type of color, e.g. 'by', 'm1', 'c1'
-                'color' - ufloat, observed color with standard error
-                'zp' - ufloa, zero-point and standard error
-                'wave' - array, wavelengths for response function, ** in angstrom **
-                'resp' - array, response function
-                'vega_zp' - dictionary, contains zero-points of the component passbands, e.g. u,b,v,y, from Vega (?)
-        :type colors_data: list or none
+                * tag: string
+                    Photometric color name/label (must be unique), e.g. '(b-y)_1'
+                * type: string
+                    Type of color. Currently supported are:
+                        * Stromgren: 'by', 'm1', 'c1'
+                        * Gaia: 'BPRP'
+                * color: `uncertainties.ufloat`
+                    Observed color with standard error.
+                * zp: `uncertainties.ufloat`
+                    Colour zero-point and standard error.  # TODO: Include help for this.
+                * wave: array_like
+                    Wavelength array for response function, in angstrom
+                * resp: array_like
+                    Response function
+                * vega_zp: dict
+                    Contains zero-points of the component passbands, e.g. u,b,v,y, from Vega # TODO: Make clearer
         """
 
         self.name = name
 
         # Zero-point data as a dictionary
-        # Error in "zero-point" for GALEX FUV & NUV are RMS from Camarota and Holberg, 2014
-        # N.B. For WISE, these are offsets from vega to AB magnitudes from Jarret et al.
-        # "This Vega basis has an overall systematic uncertainty of ∼1.45%." (0.016 mag)
+        # Error in "zero-point" for GALEX FUV & NUV are RMS from Camarota & Holberg, 2014
+        # N.B. For WISE, these are offsets from Vega to AB magnitudes from Jarret et al.
+        #   "This Vega basis has an overall systematic uncertainty of ∼1.45%." (0.016 mag)
         self.zp = {
             'FUV': ufloat(-48.60, 0.134),
             'NUV': ufloat(-48.60, 0.154),
-            # 'G': ufloat(25.6914, 0.0011),  # Values for Gaia DR2
-            # 'BP': ufloat(25.3488, 0.0005),  # Values for Gaia DR2
-            # 'RP': ufloat(24.7627, 0.0035),  # Values for Gaia DR2
             'G': ufloat(25.8010, 0.0028),
             'BP': ufloat(25.3540, 0.0023),
             'RP': ufloat(24.7627, 0.0016),
@@ -72,6 +86,7 @@ class Flux2mag:
             'H': 4.705019995520602e-11,
             'Ks': 2.4274737135123822e-11
         }
+
         # Response functions as a dictionary of interpolating functions
         R = dict()
 
@@ -92,16 +107,14 @@ class Flux2mag:
             R[b] = interp1d(wtmp, rtmp, bounds_error=False, fill_value=0)
             w_pivot[b] = wp(wtmp, rtmp)
 
-        # GALEX - source needed
+        # GALEX
         for b in ['FUV', 'NUV']:
             t = Table.read('Response/EA-{}_im.tbl'.format(b.lower()), format='ascii', names=['w', 'A'])
             R[b] = interp1d(t['w'], t['A'], bounds_error=False, fill_value=0)
             w_pivot[b] = wp(t['w'], t['A'])
 
-        # Gaia DR2 - revised band passes
+        # Gaia EDR3
         names = ['wave', 'G', 'e_G', 'BP', 'e_BP', 'RP', 'e_RP']
-        # t = Table.read('Response/GaiaDR2_RevisedPassbands.dat',  # Values for Gaia DR2
-        #                format='ascii', names=names)
         t = Table.read('Response/GaiaEDR3_passbands.dat',
                        format='ascii', names=names)
         w = t['wave'] * 10
@@ -110,7 +123,7 @@ class Flux2mag:
             R[b] = interp1d(w[i], t[b][i], bounds_error=False, fill_value=0)
             w_pivot[b] = wp(w[i], t[b][i])
 
-        # 2MASS - source needed
+        # 2MASS
         for k, b in enumerate(['J', 'H', 'Ks']):
             t = Table.read('Response/sec6_4a.tbl{:1.0f}.dat'.format(k + 1), format='ascii', names=['w', 'T'])
             R[b] = interp1d(t['w'] * 1e4, t['T'], bounds_error=False, fill_value=0)
@@ -118,7 +131,7 @@ class Flux2mag:
 
         # ALLWISE - QE-based RSRs from
         # http://wise2.ipac.caltech.edu/docs/release/allsky/expsup/sec4_4h.html
-        for b in ('W1', 'W2', 'W3', 'W4'):
+        for b in ('W1', 'W2', 'W3', 'W4'):  # TODO: exclude W3, W4 if Coelho is selected
             t = Table.read("Response/RSR-{}.txt".format(b),
                            format='ascii', names=['w', 'T', 'e'])
             R[b] = interp1d(t['w'] * 1e4, t['T'], bounds_error=False, fill_value=0)
@@ -138,8 +151,10 @@ class Flux2mag:
         self.R = R
         self.w_pivot = w_pivot
 
+        # Prepare colours data (if given)
         if colors_data:
             for x in colors_data:
+                # Stromgren colours
                 if x['type'] in ['by', 'm1', 'c1']:
                     stromgren_w, stromgren_r = {}, {}
                     for m in ('u', 'v', 'b', 'y'):
@@ -166,6 +181,7 @@ class Flux2mag:
                     x['resp'] = stromgren_r
                     x['vega_zp'] = stromgren_v
 
+                # Gaia colour
                 elif x['type'] == 'BPRP':
                     names = ['wave', 'G', 'e_G', 'BP', 'e_BP', 'RP', 'e_RP']
                     t = Table.read('Response/GaiaEDR3_passbands.dat', names=names, format='ascii')
@@ -176,43 +192,50 @@ class Flux2mag:
                     x['wave'] = gaia_w
                     x['resp'] = gaia_r
                     x['zp'] = ufloat(0.0083, 0.0021)
-                    x['vega_zp'] = gaia_v  # TODO: Gaia BP-RP vega thingy
+                    x['vega_zp'] = gaia_v  # TODO: This should come from "flux integrals for reference vega spectrum"
 
             self.colors_data = colors_data
 
-        # -------------- RETRIEVE STANDARD PHOTOMETRY WITH SIMBAD + VIZIER QUERIES -------------- #
-        # Catalogue query functions for Gaia EDR3, 2MASS, GALEX and WISE
-        Vizier_r = Vizier(columns=["*", "+_r"])
-        # Use WISE All Sky values instead of ALLWISE for consistency with flux ratio
-        # calibration and because these are more reliable at the bright end for W1 and W2
-        # v = Vizier_r.query_object(name, catalog=['I/345/gaia2', 'II/311/wise', 'II/335/galex_ais'])  # Gaia DR2
-        v = Vizier_r.query_object(name, catalog=['I/350/gaiaedr3', 'II/311/wise', 'II/335/galex_ais'])
+        # -------------- RETRIEVE STANDARD PHOTOMETRY WITH VIZIER + SIMBAD QUERIES -------------- #
+        # Catalogue query functions for Gaia EDR3, 2MASS, GALEX and WISE.
+        # This uses WISE All Sky values instead of ALLWISE for consistency with flux ratio calibration and
+        # because these are more reliable at the bright end for W1 and W2.
 
-        sb = Simbad()
-        sb.add_votable_fields('flux(J)', 'flux_error(J)', 'flux(H)', 'flux_error(H)', 'flux(K)', 'flux_error(K)')
+        # Search Vizier for Gaia, WISE and GALEX magnitudes.
+        vizier_r = Vizier(columns=["*", "+_r"])
+        v = vizier_r.query_object(name, catalog=['I/350/gaiaedr3', 'II/311/wise', 'II/335/galex_ais'])
 
         obs_mag = dict()
-        # obs_mag['G'] = 0.0505 + 0.9966 * ufloat(v[0][0]['Gmag'], v[0][0]['e_Gmag'])  # Gaia DR2
-        # obs_mag['BP'] = ufloat(v[0][0]['BPmag'], v[0][0]['e_BPmag']) - 0.0026  # Gaia DR2
-        # obs_mag['RP'] = ufloat(v[0][0]['RPmag'], v[0][0]['e_RPmag']) + 0.0008  # Gaia DR2
-        obs_mag['G'] = ufloat(v[0][0]['Gmag'], v[0][0]['e_Gmag'])
-        obs_mag['BP'] = ufloat(v[0][0]['BPmag'], v[0][0]['e_BPmag'])
-        obs_mag['RP'] = ufloat(v[0][0]['RPmag'], v[0][0]['e_RPmag'])
+        try:
+            obs_mag['G'] = ufloat(v[0][0]['Gmag'], v[0][0]['e_Gmag'])
+            obs_mag['BP'] = ufloat(v[0][0]['BPmag'], v[0][0]['e_BPmag'])
+            obs_mag['RP'] = ufloat(v[0][0]['RPmag'], v[0][0]['e_RPmag'])
+        except KeyError:
+            raise AttributeError("Something went wrong reading Gaia EDR3 magnitudes. "
+                                 "Try checking star name is correct and resolved by SIMBAD")
+        for b in ['W1', 'W2', 'W3', 'W4']:
+            try:
+                obs_mag[b] = ufloat(v[1][0][f'{b}mag'], v[1][0][f'e_{b}mag'])
+            except KeyError:
+                print(f"Unable to find result for {b} band in WISE catalog (II/311/wise).")
+        for b in ['FUV', 'NUV']:
+            try:
+                obs_mag[b] = ufloat(v[2][0][f'{b}mag'], v[2][0][f'e_{b}mag'])
+            except KeyError:
+                print(f"Unable to find result for {b} band in GALEX catalog (II/335/galex_ais).")
 
-        for b in ['J', 'H', 'K', 'W1', 'W2', 'W3', 'W4']:  # TODO: Add fail-safe for when no magnitudes found.
-            if b == 'J' or b == 'H' or b == 'K':
-                sb_tab = sb.query_object(name)
+        # Search SIMBAD for 2MASS J, H, Ks magnitudes.
+        sb = Simbad()
+        sb.add_votable_fields('flux(J)', 'flux_error(J)', 'flux(H)', 'flux_error(H)', 'flux(K)', 'flux_error(K)')
+        sb_tab = sb.query_object(name)
+        for b in ['J', 'H', 'K']:
+            try:
                 if b == 'K':
                     obs_mag['Ks'] = ufloat(sb_tab['FLUX_{}'.format(b)][0], sb_tab['FLUX_ERROR_{}'.format(b)][0])
                 else:
                     obs_mag[b] = ufloat(sb_tab['FLUX_{}'.format(b)][0], sb_tab['FLUX_ERROR_{}'.format(b)][0])
-            else:
-                obs_mag[b] = ufloat(v[1][0]['{}mag'.format(b)], v[1][0]['e_{}mag'.format(b)])
-        try:
-            obs_mag['FUV'] = ufloat(v[2][0]['FUVmag'], v[2][0]['e_FUVmag'])
-            obs_mag['NUV'] = ufloat(v[2][0]['NUVmag'], v[2][0]['e_NUVmag'])
-        except IndexError as error:
-            print(error, ': No GALEX photometry found')
+            except KeyError:
+                print(f"Unable to find result for {b} band via SIMBAD search.")
 
         # Add magnitudes from extra_data
         if extra_data:
@@ -230,25 +253,39 @@ class Flux2mag:
         else:
             self.obs_col = None
 
-    def __call__(self, wave, f_lambda, sig_ext=0, sig_col=0):
+    def __call__(self, wave, f_lambda, sig_ext=0, sig_col=0, apply_colors=False):
         """
         Integrates flux over the defined passbands and returns chi-square of fit to observed magnitudes.
 
-        :param wave: Wavelength range over which the flux is defined, in Angstrom
-        :param f_lambda: Must be call-able with argument lambda = wavelength in Angstrom
-        :param sig_ext: Amount of external noise to the magnitudes
-        :param sig_col: Amount of external noise to the colors
-        :return: chi-square of the fit to observed magnitudes, and log likelihoods
+        Parameters
+        ----------
+        wave: `synphot.SourceSpectrum.waveset`  # TODO: check this
+            Wavelength range over which the flux is defined, in Angstrom
+        f_lambda: array_like  # TODO: check this
+            Must be call-able with argument lambda = wavelength in Angstrom
+        sig_ext: float, optional
+            Amount of external noise to the magnitudes
+        sig_col: float, optional
+            Amount of external noise to the colors
+        apply_colors: bool. optional
+            Whether to include colours in the chi-square fit to observed magnitudes
+
+        Returns
+        -------
+        Chi-square of the fit to observed magnitudes, and log likelihoods
         """
 
         syn_mag = dict()
 
+        # Calculate synthetic magnitude for GALEX bands
         for b in ['FUV', 'NUV']:
-            R = self.R[b]
-            f_nu = (simps(f_lambda * R(wave) * wave, wave) /
-                    simps(R(wave) * 2.998e10 / (wave * 1e-8), wave))
-            syn_mag[b] = -2.5 * np.log10(f_nu) + self.zp[b]
+            if self.R[b]:
+                R = self.R[b]
+                f_nu = (simps(f_lambda * R(wave) * wave, wave) /
+                        simps(R(wave) * 2.998e10 / (wave * 1e-8), wave))
+                syn_mag[b] = -2.5 * np.log10(f_nu) + self.zp[b]
 
+        # Calculate synthetic magnitude for Gaia bands
         p_a = 0.7278
         hc9 = 1.986445824e-16  # 10^9 hc
         # /10000 is conversion from erg/s/cm^2/A to W/m^2/nm
@@ -256,24 +293,26 @@ class Flux2mag:
             photon_flux = p_a * simps(self.R[b](wave) * wave * f_lambda / 10000, wave) / hc9
             syn_mag[b] = -2.5 * np.log10(photon_flux) + self.zp[b]
 
+        # Calculate synthetic magnitude for 2MASS bands
         # "+20" to account for wave in A not um
         for b in ['J', 'H', 'Ks']:
-            v = self.f_vega[b]
-            zp = self.zp[b]
-            R = self.R[b]
-            syn_mag[b] = -2.5 * np.log10(simps(R(wave) * f_lambda * wave, wave) / v) + 20 + zp
+            if self.R[b]:
+                v = self.f_vega[b]
+                zp = self.zp[b]
+                R = self.R[b]
+                syn_mag[b] = -2.5 * np.log10(simps(R(wave) * f_lambda * wave, wave) / v) + 20 + zp
 
+        # Calculate synthetic magnitude for WISE bands
         # For ALLWISE, calculate AB magnitudes and then convert Vega magnitudes using corrections from
-        # Jarrett_2011_ApJ_735_112,
-        # "Conversion to the monochromatic AB system entails an additional 2.699, 3.339, 5.174, and 6.620 added
-        #  to the Vega magnitudes for W1, W2, W3, and W4, respectively"
-
-        for b in ('W1', 'W2', 'W3', 'W4'):  # TODO: exclude W3, W4 if Coelho is selected
-            R = self.R[b]
-            f_nu = (simps(f_lambda * R(wave) * wave, wave) /
-                    simps(R(wave) * 2.998e10 / wave, wave))
-            syn_mag[b] = -2.5 * np.log10(f_nu) + 20 - 48.60 - self.zp[b]  # "+20" to account for wave in A not um
-        self.syn_mag = syn_mag
+        # Jarrett_2011_ApJ_735_112: "Conversion to the monochromatic AB system entails an additional 2.699, 3.339,
+        # 5.174, and 6.620 added to the Vega magnitudes for W1, W2, W3, and W4, respectively"
+        for b in ('W1', 'W2', 'W3', 'W4'):
+            if self.R[b]:
+                R = self.R[b]
+                f_nu = (simps(f_lambda * R(wave) * wave, wave) /
+                        simps(R(wave) * 2.998e10 / wave, wave))
+                syn_mag[b] = -2.5 * np.log10(f_nu) + 20 - 48.60 - self.zp[b]  # "+20" to account for wave in A not um
+            self.syn_mag = syn_mag
 
         # Process extra_data
         if self.extra_data:
@@ -285,15 +324,15 @@ class Flux2mag:
                 syn_mag[b] = -2.5 * np.log10(f_nu) + self.zp[b]
 
         # Process colors_data
-        if self.colors_data:
+        if apply_colors and self.colors_data:
             syn_col = {}
             for x in self.colors_data:
                 b = x['tag']
                 mag = {}
                 f_interp = interp1d(wave, f_lambda)
 
+                # Calculates the Stromgren synthetic colours
                 if b in ['by', 'm1', 'c1']:
-                    # Calculates the Stromgren synthetic colours
                     for m in ('u', 'v', 'b', 'y'):
                         mag[m] = -2.5 * log10(simps(x['resp'][m] * f_interp(x['wave'][m]),
                                                     x['wave'][m]) / x['vega_zp'][m])
@@ -310,10 +349,10 @@ class Flux2mag:
                 elif b == 'BPRP':
                     zp = {'BP': ufloat(25.3540, 0.0023),
                           'RP': ufloat(24.7627, 0.0016)}
+
+                    # Calculates the Gaia synthetic colour
                     r = {}
-                    p_a = 0.7278
-                    hc9 = 1.986445824e-16  # 10^9 hc
-                    for m in ['BP', 'RP']:  # TODO check this is correct
+                    for m in ['BP', 'RP']:  # TODO check this is correct - haven't used 'vega_zp'
                         i = (x['resp'] < 99).nonzero()
                         r[m] = interp1d(x['wave'][i], x['resp'][i], bounds_error=False, fill_value=0)
                         photon_flux = p_a * simps(r[m](wave) * wave * f_lambda(wave) / 10000, wave) / hc9
@@ -324,15 +363,17 @@ class Flux2mag:
         else:
             self.syn_col = None
 
-        lnlike_m, lnlike_c = 0, 0
-        chisq = 0
-        for k, v in zip(self.syn_mag.keys(), self.syn_mag.values()):
+        # Compare observed and synthetic magnitudes and colours and calculate lnlike and fit chi-squared
+        lnlike_m, chisq = 0, 0
+        for k in self.syn_mag:
             z = self.obs_mag[k] - self.syn_mag[k]
             wt = 1 / (z.s ** 2 + sig_ext ** 2)
             chisq += z.n ** 2 * wt
             lnlike_m += -0.5 * (z.n ** 2 * wt - np.log(wt))
-        if self.syn_col:
-            for k, v in zip(self.syn_col.keys(), self.syn_col.values()):
+
+        if apply_colors and self.syn_col:
+            lnlike_c = 0
+            for k in self.syn_col:
                 z = self.obs_col[k] - self.syn_col[k]
                 wt = 1 / (z.s ** 2 + sig_col ** 2)
                 chisq += z.n ** 2 * wt
